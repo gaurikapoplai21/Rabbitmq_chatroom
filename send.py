f271c460eeaaea3fdd023cc8ad2a79fdf811d9a3
gaurika poplai <gaurikapoplai@gmail.com>
2021-04-17 23:39:36 +0530

    continuously publish messages

--- send.py
+++ send.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
-import pika
+import os,sys,pika
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+
+def main():
+   while(1):
+    str = input()
+    
+    channel.basic_publish(exchange='', routing_key='hello', body=str)
+    print(" [x] Sent ",str)
+   
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        connection.close()
+        print('Interrupted')
+        
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
